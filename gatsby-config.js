f89656def9091ef0947833fbb8930dbef235f3c6
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2022-10-13 20:59:41 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "UA-78165014-1"
+    }
+  }]
+};
