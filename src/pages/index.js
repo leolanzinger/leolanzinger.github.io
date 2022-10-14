0d7e48b4af43214139c9c01f3ab83d2a408b96a8
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2022-10-14 09:12:49 +0200

    fixed typos pt2

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,12 @@ const IndexPage = () => {
         </div>
         <div className="box-12 main">
           <p>
-            I am a product designer with a passion for user research and design strategy. I work as a Senior Product Designer at Zalando Recommerce, designing experiences for people who buy and sell second-hand fashion.
+            I am a product designer with a passion for user research and design strategy.
+            I work as a Senior Product Designer at Zalando Recommerce, designing experiences for people who buy and sell second-hand fashion.
           </p>
           <p>
-            Previously, I was part of The Studio at Zalando, a design team with a focus on early stage ideas and strategic design propositions. I also worked for Babbel in Berlin and Belka in Trento, Italy.
+            Previously, I was part of The Studio at Zalando, a design team with a focus on early stage ideas and strategic design propositions.
+            I also worked for Babbel in Berlin and Belka in Trento, Italy.
           </p>
           <p>
             Read more about my work <a href="/work">here</a>.
@@ -30,7 +32,8 @@ const IndexPage = () => {
         </div>
         <div className="box-12 main">
           <p>
-            I have a degree in Computer Science and Human-Computer interaction. When I am not glued to Figma or running user interviews, I produce and record music with <a href="https://open.spotify.com/artist/6M2Pk85GotQvfbYCmWhEme">Bob and the Apple</a> and <a href="https://open.spotify.com/artist/42MCPv6PC71ywUF6cR5uHU">Julia Pígali</a>, playing the bass guitar or the synthesiser.
+            I have a degree in Computer Science and Human-Computer Interaction.
+            When I am not glued to Figma or running user interviews, I produce and record music with <a href="https://open.spotify.com/artist/6M2Pk85GotQvfbYCmWhEme">Bob and the Apple</a> and <a href="https://open.spotify.com/artist/42MCPv6PC71ywUF6cR5uHU">Julia Pígali</a>, playing the bass guitar and the synthesiser.
           </p>
         </div>
         <Footer />
--- src/pages/work.js
+++ src/pages/work.js
@@ -20,23 +20,25 @@ const WorkPage = () => {
                 </div>
                 <div className="box-12 main">
                     <h2>Recommerce at Zalando</h2>
-                    <date>2021-2022</date>
+                    <date>2021-current</date>
                     <p>
-                        As the Senior Product Designer of the team, I am the responsible designer for the experience of 4M Zalando customers who shop and sell second-hand fashion, as well as the experience of customers using Zalando to recycle their old ripped-off clothes.
+                        As the Senior Product Designer of the team, I am the responsible designer for the experience of Zalando customers who shop and sell second-hand fashion, as well as the experience of customers using Zalando for recycling their old unwearable clothes.
                     </p>
                 </div>
                 <div className="box-12 main">
                     <h2>The Studio at Zalando</h2>
                     <date>2017-2021</date>
                     <p>
-                        Together with The Studio team, I explored different strategic topics including Zalando’s sustainability digital experience, connecting brick and mortar stores to the Zalando platform and visualising the partnership with Sephora and Nike. Being a bit of design process geek, I dived into design ops topics and managed the design community methods toolkit, designed the <a href="https://medium.com/zalando-design/zalandos-design-system-contribution-model-73ab36f8591e">design system contribution model</a> and developed a Figma plugin to help designers create prototype faster. During a brief stint in the design system team, I filled a lack of team resources by helping out as a front-end developer.
+                        Together with The Studio team, I explored different strategic topics including Zalando’s sustainability digital experience, connecting brick and mortar stores to the Zalando platform and visualising the partnership with Sephora and Nike.
+                        Being a bit of a design process geek, I dived into design ops topics and managed the design community methods toolkit, designed the <a href="https://medium.com/zalando-design/zalandos-design-system-contribution-model-73ab36f8591e">design system contribution model</a> and developed a Figma plugin to help colleagues create prototypes faster.
+                        During a brief stint in the design system team, I also helped out as a front-end developer.
                     </p>
                 </div>
                 <div className="box-12 main">
                     <h2>Babbel</h2>
                     <date>2016</date>
                     <p>
-                        As an interaction designer intern, I redesigned the sound recording tool used by language experts to record audio material for Babbel online language courses.
+                        As an interaction designer intern, my responsibility was to redesign the sound recording tool used by language experts to record audio material for Babbel online language courses.
                     </p>
                 </div>
                 <div className="box-12 main">
