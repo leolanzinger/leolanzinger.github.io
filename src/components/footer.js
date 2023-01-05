3bcacd490fa82f638b05c4d169266ce77aa48f55
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2023-01-05 12:28:21 +0100

    reformatted portfolio

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,11 @@ import "../styles/style.css"
 
 const Footer = () => {
     return (
-        <div className="box-12 main footer">
-            <h3>
-                Let’s get in touch!
-            </h3>
-            <a href="mailto:leonardo.lanzinger@gmail.com" className="social-link">Email</a>
-            <a href="https://twitter.com/Leo_Lanzinger" className="social-link">Twitter</a>
-            <a href="https://www.linkedin.com/in/leonardolanzinger/" className="social-link">Linkedin</a>
+        <div className="footer">
+            <h4>contacts</h4>
+            <p>
+                Let’s get in touch via <a href="mailto:leonardo.lanzinger@gmail.com" className="social-link">email</a>, <a href="https://twitter.com/Leo_Lanzinger" className="social-link">Twitter</a> or <a href="https://www.linkedin.com/in/leonardolanzinger/" className="social-link">Linkedin</a>.
+            </p>
         </div>
     )
 }
--- src/pages/work.js
+++ src/pages/work.js
@@ -5,7 +5,7 @@ import "../styles/work.css"
 
 const mainStyles = {
     color: "#222222",
-    fontFamily: "DM Sans, Helvetica Neue, helvetica, sans-serif, serif"
+    fontFamily: "neue-haas-grotesk-display, Helvetica Neue, helvetica, sans-serif, serif"
   }
 
 const WorkPage = () => {
@@ -13,38 +13,32 @@ const WorkPage = () => {
         <main style={mainStyles}>
             <div className="grid work">
                 <div className="box-12 main">
-                    <a className="nav" href="/">Leonardo Lanzinger</a>
+                    <a className="no-italics" href="/">Leonardo Lanzinger</a>
                 </div>
-                <div className="box-12 main">
-                    <h1>Work</h1>
-                </div>
-                <div className="box-12 main">
-                    <h2>Recommerce at Zalando</h2>
-                    <date>2021-current</date>
+                <div className="box-7 main">
+                    <h4>work</h4>
+                    <h3 className="no-top-margin">Recommerce at Zalando</h3>
+                    <date>2021 - current</date>
                     <p>
                         As the Senior Product Designer of the team, I am the responsible designer for the experience of Zalando customers who shop and sell second-hand fashion, as well as the experience of customers using Zalando for recycling their old unwearable clothes.
                     </p>
-                </div>
-                <div className="box-12 main">
-                    <h2>The Studio at Zalando</h2>
-                    <date>2017-2021</date>
+                    <h3>The Studio at Zalando</h3>
+                    <date>2017 - 2021</date>
                     <p>
                         Together with The Studio team, I explored different strategic topics including Zalando’s sustainability digital experience, connecting brick and mortar stores to the Zalando platform and visualising the partnership with Sephora and Nike.<br />
                         Being a bit of a design process geek, I dived into design ops topics and managed the design community methods toolkit, designed the <a href="https://medium.com/zalando-design/zalandos-design-system-contribution-model-73ab36f8591e">design system contribution model</a> and developed a Figma plugin to help colleagues create prototypes faster.
                         During a brief stint in the design system team, I also helped out as a front-end developer.
                     </p>
-                </div>
-                <div className="box-12 main">
-                    <h2>Babbel</h2>
+                    <h3>Babbel</h3>
                     <date>2016</date>
                     <p>
                         As an interaction designer intern, my responsibility was to redesign the sound recording tool used by language experts to record audio material for Babbel online language courses.
                     </p>
                 </div>
-                <div className="box-12 main">
-                    <hr/>
+                <div className="box-1"></div>
+                <div className="box-4 main">
+                    <Footer />
                 </div>
-                <Footer />
             </div>
         </main>
   )
