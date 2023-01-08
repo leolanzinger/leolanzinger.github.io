c756bc169a99c4adc2051037cc68e53c6949e48a
Leonardo Lanzinger <llanzinger@zalando-70884.speedport.ip>
2023-01-08 14:04:12 +0100

    added onboarding animation

--- src/components/animationCanvas.js
+++ src/components/animationCanvas.js
@@ -4,15 +4,24 @@ import "../styles/style.css"
 import frameRenderer from "./framerender";
 
 const AnimationCanvas = () => {
-    const baseRadius = 2;
-    const colors = ["#F1C40F", "#1ABC9C", "#3498DB", "#9B59B6", "#95A5A6"];
-    const ballRef = useRef({ x: 50, y: 50, vx: 3.9, vy: 3.9, radius: baseRadius, color: colors[0] });
-    const canvasRef = useRef(null);
+    // onboarding animation variable
+    let onboarding = true;
+    let bounces = 0;
+    let canvasIsAnimated = false;
+    // canvas variables
     let size = {
         width: 0,
         height: 0
     }
     let mouseUp = true;
+    // ball variables
+    const baseRadius = 2;
+    const colors = ["#F1C40F", "#1ABC9C", "#3498DB", "#9B59B6", "#95A5A6"];
+    // refs
+    const ballRef = useRef({ x: 50, y: 50, vx: 3.9, vy: 3.9, radius: baseRadius, color: colors[0] });
+    const canvasRef = useRef(null);
+    const requestIdRef = useRef(null);
+    const intervalRef = useRef(null);
 
     // MOUSE HANDLING
     const handleMouseMove = (event) => {
@@ -21,20 +30,20 @@ const AnimationCanvas = () => {
         const ball = ballRef.current;
         ball.x = x;
         ball.y = y;
+        if (onboarding) {
+            requestIdRef.current = requestAnimationFrame(animateOnboarding);
+        }
         tick();
     }
-
     const handleMouseDown = () => {
         mouseUp = false;
-        requestAnimationFrame(increaseBall);
+        onboarding = false;
+        requestIdRef.current = requestAnimationFrame(increaseBall);
     }
-
     const handleMouseUp = () => {
         mouseUp = true;
         const ball = ballRef.current;
-        console.log("current color: ", ball.color);
-        ball.color = colors[Math.floor(Math.random() * 5)];
-        console.log("new color: ", ball.color);
+        ball.color = randomColor();
         resetBall();
     }
 
@@ -42,19 +51,66 @@ const AnimationCanvas = () => {
     const increaseBall = () => {
         const ball = ballRef.current;
         ball.radius += 4;
-        if (!mouseUp) requestAnimationFrame(increaseBall);
+        if (ball.radius > 10) canvasIsAnimated = true;
+        if (!mouseUp) requestIdRef.current = requestAnimationFrame(increaseBall);
     }
-
     const resetBall = () => {
         const ball = ballRef.current;
         // bounce animation
         if (ball.radius > baseRadius) {
             ball.radius -= (ball.radius / 6);
-            requestAnimationFrame(resetBall);
+            requestIdRef.current = requestAnimationFrame(resetBall);
         }
         else {
             ball.radius = baseRadius;
         }
+    }
+    const inflateBall = () => {
+        const ball = ballRef.current;
+        if (ball.radius < 20) {
+            ball.radius += 4;
+            requestIdRef.current = requestAnimationFrame(inflateBall);
+            tick();
+        }
+        else {
+            requestIdRef.current = requestAnimationFrame(deflateBall);
+            tick();
+        }
+    }
+    const deflateBall = () => {
+        const ball = ballRef.current;
+        if (ball.radius > baseRadius) {
+            ball.radius -= 4;
+            requestIdRef.current = requestAnimationFrame(deflateBall);
+            tick();
+        }
+        else {
+            ball.radius = baseRadius;
+            if (bounces < 2) {
+                bounces++;
+                requestIdRef.current = requestAnimationFrame(inflateBall);
+            }
+            else {
+                ball.color = randomColor();
+            }
+            tick();
+        }
+    }
+    const animateOnboarding = () => {
+        onboarding = false;
+        bounces = 0;
+        intervalRef.current = setInterval(function() {
+            if (!canvasIsAnimated) {
+                bounces = 0;
+                requestIdRef.current = requestAnimationFrame(inflateBall);
+            }
+            else {
+                clearInterval(intervalRef.current);
+            }
+        }, 3600);
+    }
+    const randomColor = () => {
+        return colors[Math.floor(Math.random() * 5)];
     }
 
     // FRAME RENDERING
@@ -67,7 +123,7 @@ const AnimationCanvas = () => {
     const tick = () => {
         if (!canvasRef.current) return;
         renderFrame();
-        requestAnimationFrame(tick);
+        requestIdRef.current = requestAnimationFrame(tick);
     };
 
     // ON MOUNT
@@ -80,10 +136,9 @@ const AnimationCanvas = () => {
             canvasRef.current.width = size.width;
             canvasRef.current.height = size.height;
         }
-        // requestIdRef.current = requestAnimationFrame(tick);
         window.addEventListener("resize", () => handleResize());
         return () => {
-        //   cancelAnimationFrame(requestIdRef.current);
+        cancelAnimationFrame(requestIdRef.current);
           window.removeEventListener("resize", handleResize);
         };
       }, []);
