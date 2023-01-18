1da9a63b80c96c515c4dda4e1f8f3224839915d6
Leonardo Lanzinger <llanzinger@zalando-70884.speedport.ip>
2023-01-19 00:04:56 +0100

    desktop web redesign

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,40 +4,73 @@ import AnimationCanvas from "../components/AnimationCanvas";
 import "../styles/style.css"
 
 const mainStyles = {
-  color: "#222222",
+  color: "#292929",
   fontFamily: "neue-haas-grotesk-display, helvetica, sans-serif, serif",
-  pointerEvents: "none"
+  // pointerEvents: "none"
 }
 
 const IndexPage = () => {
   return (
     <div>
-      <AnimationCanvas />
+      {/* <AnimationCanvas /> */}
       <main style={mainStyles} className="homepage">
-        <div className="grid">
-          <div className="box-7 main title">
+        <div className="grid border-top border-bottom">
+          <div className="box border-right box-6 main title padding-content">
             <h1>Leonardo Lanzinger</h1>
-            <h4 className="subtitle">digital product designer</h4>
+            <div className="subtitle-wrapper">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="60"
+                height="60"
+                viewBox="0 0 60 60"
+                fill="none">
+                <circle cx="30" cy="30" r="30" fill="url(#paint0_linear_118_40)"/>
+                <defs>
+                  <linearGradient id="paint0_linear_118_40" x1="36" y1="0" x2="36" y2="72" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#DA291C"/>
+                    <stop offset="1" stop-color="#5295BB"/>
+                  </linearGradient>
+                </defs>
+              </svg>
+              <h4 className="subtitle">
+                digital
+                <br />
+                product
+                <br />designer
+              </h4>
+            </div>
           </div>
-          <div className="box-5 main">
-            <h4>work</h4>
-            <p>
-              I am a product designer with a passion for user research and design strategy.
-              I work as a Principal Product Designer at Zalando Recommerce, designing experiences for people who buy and sell second-hand fashion.
-            </p>
-            <p>
-              Previously, I was part of The Studio at Zalando, a design team with a focus on early stage ideas and strategic design propositions.
-              I also worked for Babbel in Berlin and Belka in Trento, Italy.
-            </p>
-            <p>
-              Read more about my work <a href="/work">here</a>.
-            </p>
-            <h4>education + music</h4>
-            <p>
-              I have a bachelor degree in Computer Science and a master in Human-Computer Interaction Design.
-              When I am not glued to Figma or running user interviews, I produce and record music with <a href="https://open.spotify.com/artist/6M2Pk85GotQvfbYCmWhEme">Bob and the Apple</a> and <a href="https://open.spotify.com/artist/42MCPv6PC71ywUF6cR5uHU">Julia Pígali</a>, playing the bass guitar and the synthesiser.
-            </p>
-            <Footer />
+          <div className="box-6 main">
+            <div className="border-bottom padding-right">
+              <div className="border-right padding-content">
+                <h4>work</h4>
+                <p>
+                  I am a product designer with a passion for user research and design strategy.
+                  I work as a Principal Product Designer at Zalando Recommerce, designing experiences for people who buy and sell second-hand fashion.
+                </p>
+                <p>
+                  Previously, I was part of The Studio at Zalando, a design team with a focus on early stage ideas and strategic design propositions.
+                  I also worked for Babbel in Berlin and Belka in Trento, Italy.
+                </p>
+                <p>
+                  Read more about my work <a href="/work">here</a>.
+                </p>
+              </div>
+            </div>
+            <div className="border-bottom padding-right">
+              <div className="border-right padding-content">
+                <h4>education + music</h4>
+                <p>
+                  I have a bachelor degree in Computer Science and a master in Human-Computer Interaction Design.
+                  When I am not glued to Figma or running user interviews, I produce and record music with <a href="https://open.spotify.com/artist/6M2Pk85GotQvfbYCmWhEme">Bob and the Apple</a> and <a href="https://open.spotify.com/artist/42MCPv6PC71ywUF6cR5uHU">Julia Pígali</a>, playing the bass guitar and the synthesiser.
+                </p>
+              </div>
+            </div>
+            <div className="padding-right">
+              <div className="border-right padding-content">
+                <Footer />
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- src/pages/work.js
+++ src/pages/work.js
@@ -2,6 +2,9 @@ import * as React from "react"
 import Footer from "../components/footer"
 import "../styles/style.css"
 import "../styles/work.css"
+import recomImage from "../images/recom.png"
+import studioImage from "../images/studio.png"
+import babbelImage from "../images/babbel.png"
 
 const mainStyles = {
     color: "#222222",
@@ -11,39 +14,85 @@ const mainStyles = {
 const WorkPage = () => {
     return (
         <main style={mainStyles}>
-            <div className="grid work">
-                <div className="box-12 main">
-                    <a className="no-italics" href="/">Leonardo Lanzinger</a>
-                </div>
-                <div className="box-7 main">
+            <div className="grid work border-top border-bottom">
+              <div className="box-12 padding-left padding-right">
+                <div className="grid">
+                  <div className="box-6 padding-content">
                     <h4>work</h4>
+                  </div>
+                  <div className="box-6 padding-content right-align">
+                    <h4><a href="/">Leonardo Lanzinger</a></h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid work border-bottom">
+              <div className="box-12 padding-left">
+                <div className="grid">
+                  <div className="box-5 padding-content">
                     <h3 className="no-top-margin">Recommerce at Zalando</h3>
                     <date>2021 - current</date>
                     <p>
-                        Principal Product Designer, designing the customer experience of Zalando customers who shop and sell second-hand fashion, as well as the experience of customers using Zalando for recycling their old unwearable clothes.
+                    As Principal Product Designer, I worked on the customer experience of Zalando customers who shop and sell second-hand fashion, as well as the experience of customers using Zalando for recycling their old unwearable clothes.
                     </p>
                     <p>
-                        Past and ongoing projects include: leading user research to identify strategic opportunities in the overall customer journey, product design work on the existing second-hand service as well as on an upcoming recycling service and workshops facilitation.
+                    Past and ongoing projects include: leading user research to identify strategic opportunities in the overall customer journey, product design work on the existing second-hand service as well as on an upcoming recycling service and workshops facilitation.
                     </p>
-                    <h3>The Studio at Zalando</h3>
+                  </div>
+                  <div className="box-1 border-right"></div>
+                  <div className="box-6 padding-content margin-right border-right">
+                    <img src={recomImage} alt="recommerce" className="case-study" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid work border-bottom">
+              <div className="box-12 padding-left">
+                <div className="grid">
+                  <div className="box-5 padding-content">
+                    <h3 className="no-top-margin">The Studio at Zalando</h3>
                     <date>2017 - 2021</date>
                     <p>
-                        Together with The Studio team, I explored different strategic topics including Zalando’s sustainability digital experience, connecting brick and mortar stores to the Zalando platform and visualising the partnership with Sephora and Nike.<br />
+                      Together with The Studio team, I explored different strategic topics including Zalando’s sustainability digital experience, connecting brick and mortar stores to the Zalando platform and visualising the partnership with Sephora and Nike.<br />
                     </p>
                     <p>
-                        Being a bit of a design process geek, I dived into design ops topics and managed the design community methods toolkit, designed the <a href="https://www.youtube.com/watch?v=uTZAErqLPd8">design system contribution model</a> and developed a Figma plugin to help colleagues create prototypes faster.
-                        During a brief stint in the design system team, I also helped out as a front-end developer to develop the newly designed components.
+                      Being a bit of a design process geek, I dived into design ops topics and managed the design community methods toolkit, designed the <a href="https://www.youtube.com/watch?v=uTZAErqLPd8">design system contribution model</a> and developed a Figma plugin to help colleagues create prototypes faster.
+                      During a brief stint in the design system team, I also helped out as a front-end developer to develop the newly designed components.
                     </p>
-                    <h3>Babbel</h3>
+                  </div>
+                  <div className="box-1 border-right"></div>
+                  <div className="box-6 padding-content margin-right border-right">
+                    <img src={studioImage} alt="thestudio" className="case-study" />
+                    <caption>
+                      Some Zalando projects I worked on between 2017 and 2021.
+                      <br />
+                      Illustration credits to my amazing communication design colleagues.
+                    </caption>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid work border-bottom">
+              <div className="box-12 padding-left">
+                <div className="grid">
+                  <div className="box-5 padding-content">
+                    <h3 className="no-top-margin">Babbel</h3>
                     <date>2016</date>
                     <p>
-                        As an interaction designer intern, my responsibility was to redesign the sound recording tool used by language experts to record audio material for Babbel online language courses, integrating it iin a git-based workflow.
+                    As an interaction designer intern, I redesigned the sound recording tool used by language experts to record audio material for Babbel online language courses, integrating it in a git-based CAT tool.
                     </p>
+                  </div>
+                  <div className="box-1 border-right"></div>
+                  <div className="box-6 padding-content margin-right border-right">
+                    <img src={babbelImage} alt="babbel" className="case-study" />
+                  </div>
                 </div>
-                <div className="box-1"></div>
-                <div className="box-4 main">
-                    <Footer />
-                </div>
+              </div>
+            </div>
+            <div className="grid work border-bottom padding-left">
+              <div className="box-12 border-right padding-content">
+                <Footer />
+              </div>
             </div>
         </main>
   )
