84c3c1c6715bfee1d45ed24159ae53ff6368ffb3
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2023-07-04 23:38:41 +0300

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom"
 import { AnimatePresence } from "framer-motion"
 import Root from "./routes/root"
-import Work from "./routes/work"
-import About from "./routes/about"
-import Contacts from "./routes/contacts"
 
 export default function App() {
   const location = useLocation()
@@ -13,9 +10,6 @@ export default function App() {
       <Routes location={location} key={location.pathname}>
         {/* Routes */}
         <Route index element={<Root />} />
-        <Route path="Work" element={<Work />} />
-        <Route path="About" element={<About />} />
-        <Route path="Contacts" element={<Contacts />} />
         {/* 404 page */}
         {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
